761d222e3cc94038b2e8c333569a6019254e0795
Jeff9x <jeff.ngara@empirefx.com>
2025-07-28 12:01:55 +0300

    triggering github actions build

---
+++ Jenkinsfile/Jenkinsfile.groovy
@@ -0,0 +1,66 @@
+// Jenkinsfile
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_HUB_USERNAME = "jeffare9x"
+        KUBE_NAMESPACE = "default"
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Build & Push App One') {
+            steps {
+                script {
+                    def fullImageName = "${env.jeffare9x}/app-one"
+                    def dockerImage = docker.build(fullImageName, './app-one')
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-creds') {
+                        dockerImage.push("latest")
+                    }
+                }
+            }
+        }
+        stage('Build & Push App Two') {
+            steps {
+                script {
+                    def fullImageName = "${env.jeffare9x}/app-two"
+                    def dockerImage = docker.build(fullImageName, './app-two')
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-creds') {
+                        dockerImage.push("latest")
+                    }
+                }
+            }
+        }
+        stage('Build & Push App Three') {
+            steps {
+                script {
+                    def fullImageName = "${env.jeffare9x}/app-three"
+                    def dockerImage = docker.build(fullImageName, './app-three')
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-creds') {
+                        dockerImage.push("latest")
+                    }
+                }
+            }
+        }
+        stage('Deploy All Apps & Ingress') {
+            steps {
+                withCredentials([file(credentialsId: 'kubeconfig-file', variable: 'KUBECONFIG')]) {
+                    sh "kubectl apply -f k8s/deployment.yaml --namespace ${KUBE_NAMESPACE}"
+                }
+            }
+        }
+        stage('Verify All Deployments') {
+            steps {
+                withCredentials([file(credentialsId: 'kubeconfig-file', variable: 'KUBECONFIG')]) {
+                    sh "kubectl rollout status deployment/app-one-deployment --namespace ${KUBE_NAMESPACE}"
+                    sh "kubectl rollout status deployment/app-two-deployment --namespace ${KUBE_NAMESPACE}"
+                    sh "kubectl rollout status deployment/app-three-deployment --namespace ${KUBE_NAMESPACE}"
+                }
+            }
+        }
+    }
+}
